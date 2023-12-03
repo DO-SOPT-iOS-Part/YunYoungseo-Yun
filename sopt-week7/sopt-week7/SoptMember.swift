f8561418d9140a64a8c414f3483b30e8cecfcf20
0seoYun <yys020819@gmail.com>
2023-12-03 18:07:35 +0900

    [Week7] 아키텍쳐 패턴

---
+++ sopt-week7/sopt-week7/SoptMember.swift
@@ -0,0 +1,14 @@
+//
+//  SoptMember.swift
+//  sopt-week7
+//
+//  Created by 윤영서 on 12/2/23.
+//
+
+import Foundation
+
+struct SoptMember {
+    let name: String
+    var winning: Bool
+    var age: Int
+}
